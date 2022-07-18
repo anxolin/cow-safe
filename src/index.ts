19a1a4fe5e04fb0cd2f9840688cd01be500becb2
Anxo Rodriguez <anxolin@gmail.com>
2022-07-18 16:17:13 +0100

    Add mnemonic and dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,18 @@
+import 'dotenv/config'
+
+import { ethers, Wallet } from "ethers";
+import { strict as assert } from 'node:assert';
+
 import { CowSdk } from '@cowprotocol/cow-sdk'
 
-const chainId = 4 // Rinkeby
-const cowSdk = new CowSdk(chainId)
+const MNEMONIC = process.env.MNEMONIC
+const NETWORK = process.env.NETWORK
+
+assert(MNEMONIC, 'MNEMONIC environment var is required')
+assert(NETWORK, 'NETWORK environment var is required')
+
+const cowSdk = new CowSdk(parseInt(NETWORK))
+const wallet = Wallet.fromMnemonic(MNEMONIC)
 
 async function run() {
   const trader = '0x68ac5adfd66afa7bf79964cbb6defd91659dc54e'
